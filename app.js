26bfc1bb3cc766813a08bd9781e5529ff8f87856
kaybee1102 <kaybeee1102@gmail.com>
2022-04-20 00:01:23 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ input: rs, output: {} });
+const prefectureDataMap = new Map();
+rl.on('line', lineString => {
+  const conlumns = lineString.split(',');
+  const year = parseInt(conlumns[0]);
+  const prefecture = conlumns[1];
+  const popu = parseInt(conlumns[3]);
+  if (year === 2010 || year === 2015) { 
+    let value = null;
+    if (prefectureDataMap.has(prefecture)) { 
+      value = prefectureDataMap.get(prefecture);
+    } else {
+      value = {
+        popu10: 0,
+        popu15: 0,
+        change: null
+      };
+    }
+    if (year === 2010) {
+      value.popu10 = popu;
+    } if (year === 2015) {
+      value.popu15 = popu;
+    }
+    prefectureDataMap.set(prefecture, value);
+  };
+});
+rl.on('close', () => {
+  for (const [key, value] of prefectureDataMap) {
+    value.change = value.popu15 / value.popu10;
+  }
+  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+    return
+  });
+  const rankingStrings = rankingArray.map(([key, value]) => {
+    return (
+      `${key}: ${value.popu10}=>${value.popu15} 変化率: ${value.change}`
+    );
+  });
+  console.log(rankingStrings);
+});
